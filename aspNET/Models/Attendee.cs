6b497b80a5df42d528af4be09175c872288d1cc9
Stefan Garces <stefangarces@gmail.com>
2021-04-01 22:12:08 +0200

    Fix: Removed all from table "Attendee".

--- aspNET/Models/Attendee.cs
+++ aspNET/Models/Attendee.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace aspNET.Models
+﻿namespace aspNET.Models
 {
     public class Attendee
     {
-        public string userName { get; set; }
-        public string eMail { get; set; }
-        public string phoneNumber { get; set; }
+
     }
 }

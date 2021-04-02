1818d46d05abaf517bf15af6a19cccf98be00079
Stefan Garces <stefangarces@gmail.com>
2021-04-02 23:09:54 +0200

    Comment out app.UseAuthorization();

--- aspNET/Startup.cs
+++ aspNET/Startup.cs
@@ -50,7 +50,7 @@ namespace aspNET
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            //app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {

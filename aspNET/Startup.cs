487cabe1c936108cc049d56c4851a46cf329dc8f
Stefan Garces <stefangarces@gmail.com>
2021-04-03 20:20:36 +0200

    Add: UseAuthorization.

--- aspNET/Startup.cs
+++ aspNET/Startup.cs
@@ -1,16 +1,10 @@
+﻿using aspNET.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Diagnostics;
-using System.Threading.Tasks;
-using aspNET.Data;
-using Microsoft.EntityFrameworkCore;
 
 namespace aspNET
 {
@@ -50,7 +44,7 @@ namespace aspNET
 
             app.UseRouting();
 
-            //app.UseAuthorization();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {

21baf66c072b130060b8b058f06a1f4d6a86306a
Stefan Garces <stefangarces@gmail.com>
2021-04-05 20:52:32 +0200

    Commented out class: Organizer.

--- aspNET/Models/Organizer.cs
+++ aspNET/Models/Organizer.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace aspNET.Models
+﻿namespace aspNET.Models
 {
     public class Organizer
     {
+        /*
         public string orgName { get; set; }
         public string orgMail { get; set; }
         public string orgPhoneNumber { get; set; }
+        */
     }
 }

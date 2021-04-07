9742f224fcb68e3397ebc0b8a1c6df60a4301c23
Stefan Garces <stefangarces@gmail.com>
2021-04-07 21:02:37 +0200

    add:

--- aspNET/Models/Organizer.cs
+++ aspNET/Models/Organizer.cs
@@ -3,7 +3,9 @@
     public class Organizer
     {
 
-        public string orgName { get; set; }
+        public 
+            string orgName 
+        { get; set; }
         public string orgMail { get; set; }
         public string orgPhoneNumber { get; set; }
 

0fd716410b407b2d9a047cf6cf3f192bb59f7d6c
Stefan Garces <stefangarces@gmail.com>
2021-04-10 19:59:24 +0200

    Uncomment Events.

--- aspNET/Models/Events.cs
+++ aspNET/Models/Events.cs
@@ -1,7 +1,7 @@
 ﻿namespace aspNET.Models
 {
     public class Events
-    {/*
+    {
         public string Title { get; set; }
         public string Description { get; set; }
         public string Place { get; set; }
@@ -9,6 +9,6 @@
         public DateTime Date { get; set; }
         public bool ticketsAvailable { get; set; }
         public Organizer OrgID { get; set; }
-    */
+    
     }
 }

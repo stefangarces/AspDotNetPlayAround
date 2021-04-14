5a3757ee6653aee3588bf12e41474c567aa6afa4
Stefan Garces <stefangarces@gmail.com>
2021-04-14 16:14:41 +0200

    Outcomment.

--- aspNET/Models/Events.cs
+++ aspNET/Models/Events.cs
@@ -1,16 +1,14 @@
-﻿using System;
-
-namespace aspNET.Models
+﻿namespace aspNET.Models
 {
     public class Events
     {
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string Place { get; set; }
-        public string Adress { get; set; }
-        public DateTime Date { get; set; }
-        public bool ticketsAvailable { get; set; }
-        public Organizer OrgID { get; set; }
+        /*  public string Title { get; set; }
+          public string Description { get; set; }
+          public string Place { get; set; }
+          public string Adress { get; set; }
+          public DateTime Date { get; set; }
+          public bool ticketsAvailable { get; set; }
+          public Organizer OrgID { get; set; } */
 
     }
 }

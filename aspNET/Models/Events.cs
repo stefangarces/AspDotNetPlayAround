469dbd47548486144f7d502d31c7155aa5e6b327
Stefan Garces <stefangarces@gmail.com>
2021-04-16 10:10:42 +0200

    Add: DateTime.

--- aspNET/Models/Events.cs
+++ aspNET/Models/Events.cs
@@ -1,14 +1,16 @@
-﻿namespace aspNET.Models
+﻿using System;
+
+namespace aspNET.Models
 {
     public class Events
     {
-        /*  public string Title { get; set; }
-          public string Description { get; set; }
-          public string Place { get; set; }
-          public string Adress { get; set; }
-          public DateTime Date { get; set; }
-          public bool ticketsAvailable { get; set; }
-          public Organizer OrgID { get; set; } */
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Place { get; set; }
+        public string Adress { get; set; }
+        public DateTime Date { get; set; }
+        public bool ticketsAvailable { get; set; }
+        public Organizer OrgID { get; set; }
 
     }
 }

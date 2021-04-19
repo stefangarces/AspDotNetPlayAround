ffc9f2eaafe4983bc453213803b2c32ce2f1c6fd
Stefan Garces <stefangarces@gmail.com>
2021-04-19 22:06:18 +0200

    Uncomment

--- aspNET/Models/Events.cs
+++ aspNET/Models/Events.cs
@@ -4,7 +4,7 @@ namespace aspNET.Models
 {
     public class Events
     {
-        public string Title { get; set; }
+        //public string Title { get; set; }
         public string Description { get; set; }
         public string Place { get; set; }
         public string Adress { get; set; }
